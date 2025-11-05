d2181c0255dbcbcf33911089cab08fddb4c4fb52
raimalrajagoal@gmail.com <raimalrajagoal@gmail.com>
2025-11-05 23:33:43 +0500

    push data

--- push_data.py
+++ push_data.py
@@ -14,3 +14,45 @@ ca = certifi.where()
 import pandas as pd
 import numpy as np
 import pymongo
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+
+class NetworkDataExtract():
+    def  __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def cv_to_json_convertor(self,file_path):
+        try:
+            data = pd.read_csv(file_path)
+            data.reset_index(drop=True, inplace=True)
+            records = list(json.loads(data.T.to_json()).values())
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def insert_data_mongodb(self,records,database,collection):
+        try:
+            self.database = database
+            self.collection = collection
+            self.records = records
+            
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
+            
+            self.collection= self.database[self.collection]
+            self.collection.insert_many(self.records)
+            
+            return(len(self.records))
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+if __name__=='__main__':
+    FILE_PATH = r'D:\GitHub\MLOPs-ETL-Pipelines-Network-Security-System\Network_data\phisingData.csv'
+    DATABASE = "RAIMAL"
+    Collection = "NetworkData"
+    networkobj = NetworkDataExtract()
+    records = etworkobj.csv_to_json_convertor(file_path = FILE_PATH)
+    no_of_records = networkobj.inert_data_monogo(records, DATABASE,Collection)
+    print(records)
