ae6e905234ceb36e291f6becd1242e0e4a30e52b
raimalrajagoal@gmail.com <raimalrajagoal@gmail.com>
2025-11-12 01:18:32 +0500

    Test MongoDB

--- test_mongodb.py
+++ test_mongodb.py
@@ -8,6 +8,7 @@ client = MongoClient(uri)
 
 # Send a ping to confirm a successful connection
 try:
+    
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
