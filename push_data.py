a5e510432db3df3f27e187f82d448b8035f5f51b
raimalrajagoal@gmail.com <raimalrajagoal@gmail.com>
2025-11-12 13:02:42 +0500

    Push data to monggodb

--- push_data.py
+++ push_data.py
@@ -24,11 +24,12 @@ class NetworkDataExtract():
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def cv_to_json_convertor(self,file_path):
+    def csv_to_json_converter(self,file_path):
         try:
             data = pd.read_csv(file_path)
             data.reset_index(drop=True, inplace=True)
             records = list(json.loads(data.T.to_json()).values())
+            return records
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
@@ -53,6 +54,6 @@ if __name__=='__main__':
     DATABASE = "RAIMAL"
     Collection = "NetworkData"
     networkobj = NetworkDataExtract()
-    records = networkobj.csv_to_json_convertor(file_path = FILE_PATH)
-    no_of_records = networkobj.inert_data_monogo(records, DATABASE,Collection)
+    records = networkobj.csv_to_json_converter(file_path = FILE_PATH)
+    no_of_records = networkobj.insert_data_mongodb(records, DATABASE,Collection)
     print(records)
