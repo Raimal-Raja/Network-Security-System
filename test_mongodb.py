6ffedfbc6604f0825c81048441071b64692cc7b9
raimalrajagoal@gmail.com <raimalrajagoal@gmail.com>
2025-11-14 12:27:10 +0500

    Push Data to Mongodb Atlas

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,14 +1,13 @@
 
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://raimalraja_db_user:RK.Raimal@cluster0.yyhjlpq.mongodb.net/?appName=Cluster0"
+uri = "mongodb+srv://raimalraja_db_user:KavitaRaimal@cluster0.yyhjlpq.mongodb.net/?appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
 
 # Send a ping to confirm a successful connection
 try:
-    
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
