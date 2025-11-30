6c792cdfa92cd20da5d972515c65fa783aa83f97
raimalrajagoal@gmail.com <raimalrajagoal@gmail.com>
2025-11-30 18:47:39 +0500

    Model Trainer & Evaluation with Hyperparameter tuning

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_transformtion import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer,ModelTrainerArtifact,ModelTrainerConfig
 
 if __name__ =="__main__":
     try:
@@ -30,6 +31,16 @@ if __name__ =="__main__":
         
         print(data_transformation_artifact)
         logging.info("Data Transformation Completed!")
+        print(logging.info("Data Transformation Completed!"))
+        
+        logging.info('Model training started!')
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        
+        
+        logging.info('Model Training artifact created')
+        print(logging.info('Model Training artifact created'))
         
     except Exception as e:
         raise NetworkSecurityException(e,sys)
